c25966d6cc29ab0c66e1c5f384b9370f24dec574
Eric <zhangningyu16900@vip.qq.com>
2024-02-17 13:21:06 +0800

    Update __init__.py with code acknowledgment statement

--- workflow_data/src/__init__.py
+++ workflow_data/src/__init__.py
@@ -1,3 +1,13 @@
-"""cell2sentence"""
+"""
+This file was initially developed by the project at https://github.com/vandijklab/cell2sentence-ft. 
+Many thanks for their contributions to this field. It adheres to the Attribution-NonCommercial-ShareAlike 
+4.0 International License. 
+
+If you use this file, please cite the papers "Levine et al., Cell2Sentence: Teaching Large Language 
+Models the Language of Biology. 2023 (https://www.biorxiv.org/content/10.1101/2023.09.11.557287v3)" and 
+"Rahul M Dhodapkar. Representing cells as sentences enables natural-language processing for single-cell 
+transcriptomics. 2022 (https://www.biorxiv.org/content/10.1101/2022.09.18.508438)."
+"""
+
 __version__ = "0.0.1"
 from src.csdata import CSData
